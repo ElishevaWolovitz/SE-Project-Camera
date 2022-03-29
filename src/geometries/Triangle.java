4281d011bcc1d88870965adfe0fd1bccfef671a7
Elana <elweiss@g.jct.ac.il>
2022-03-29 22:28:46 +0300

    Updated TriangleTests, and Triangle

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,10 +1,12 @@
 package geometries;
 
 import java.util.LinkedList;
+
 import java.util.List;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
 public class Triangle extends Polygon {
 
@@ -15,29 +17,37 @@ public class Triangle extends Polygon {
 
 	@Override
 	public List<Point> findIntersections(Ray ray) {
-		List<Point> listIntersec = new LinkedList<Point>(); 
-		if ((this.plane.findIntersections(ray)) == null)
+		List<Point> planeIntersections= this.plane.findIntersections(ray);
+		
+		//if ray does not intersect with the plane the triangle lies on
+		//it does not intersect with the triangle either
+		if (planeIntersections == null)
 				return null; 
-		//if the distance between the point and 
-		//all 3 vertices is less then the distance between the 3 vertices 
-		//then the point lies in the triangle
-		double PA, PB, PC, AB, BC, CA; 
-		Point a, b, c; 
-		a = vertices.get(0); 
-		b = vertices.get(1); 
-		c = vertices.get(2); 
-		PA = ray.p0.distance(a); 
-		PB = ray.p0.distance(b); 
-		PC = ray.p0.distance(c); 
-		AB = a.distance(b); 
-		BC = b.distance(c); 
-		CA = c.distance(a); 
-		if((PA + PB + PC) < (AB+BC+CA))
+	
+		double d1,d2,d3;
+		Vector v1,v2,v3,n1,n2,n3;
+		v1=vertices.get(0).subtract(ray.p0);
+		v2=vertices.get(1).subtract(ray.p0);
+		v3=vertices.get(2).subtract(ray.p0);
+		n1= v1.crossProduct(v2).normalize();
+		n2= v2.crossProduct(v3).normalize();
+		n3= v3.crossProduct(v1).normalize();
+		//storing the intersection point in a variable
+		Point intersectionPoint= planeIntersections.get(0);
+		
+		//vector from the ray's origin to the intersection point on the plane
+		Vector toTriangle= intersectionPoint.subtract(ray.p0);
+		d1= toTriangle.dotProduct(n1);
+		d2= toTriangle.dotProduct(n2);
+		d3= toTriangle.dotProduct(n3);
+		
+		//need to check that d1,d2 and d3 are all positive or all negative
+		if ((d1>0 && d2>0 && d3>0)||(d1<0 && d2<0 && d3<0))
 		{
-			listIntersec.add(ray.p0); 
-			return listIntersec; 
+			return List.of(intersectionPoint);
 		}
-		else 
-			return null; 
+		
+		return null;
+		
 	}
 }
--- src/unittests/TriangleTests.java
+++ src/unittests/TriangleTests.java
@@ -18,7 +18,7 @@ public class TriangleTests {
 	public void testFindIntersections() {
 		Triangle triangle = new Triangle(new Point(0,5,0), new Point(0,10,0), new Point(10,5,0));
 		Ray ray;
-		List<Point> expected, actual;
+		List<Point> expected, result;
 
 		/**
 		 * @author elana
@@ -26,25 +26,54 @@ public class TriangleTests {
 		// ============ BVA ================
 		// On edge of triangle
 		ray = new Ray(new Point(5,5,10), new Vector(0,0,-1));
-		actual = triangle.findIntersections(ray);
-		assertEquals("On edge of triangle", null, actual);
+		result = triangle.findIntersections(ray);
+		assertEquals("On edge of triangle", null, result);
 
 		// In vertex of triangle
 		ray = new Ray(new Point(0,5,10), new Vector(0,0,-1));
-		actual = triangle.findIntersections(ray);
-		assertEquals("In vertex of triangle", null, actual);
+		result = triangle.findIntersections(ray);
+		assertEquals("In vertex of triangle", null, result);
 
 		// On continuation of edge
 		ray = new Ray(new Point(-5,5,10), new Vector(0,0,-1));
-		actual = triangle.findIntersections(ray);
-		assertEquals("On continuation of edge", null, actual);
+		result = triangle.findIntersections(ray);
+		assertEquals("On continuation of edge", null, result);
+			 
+			
+		/**
+		 * @author elish
+		 *
+		 */
+	     // ============ Equivalence Partitions Tests ==============
+		Point p1, p2, p3; 
+		p1 = new Point(1, 0, 0); 
+		p2 = new Point(2, 0, 0); 
+		p3 = new Point(1, -2, 0); 
+	    Vector v1;  
+	    Triangle t1 = new Triangle(p1, p2, p3);
+	    
+	        //TC01: Point of intersection inside triangle
+	     p1 = new Point(1.5, -1, -2); 
+	     p2 = new Point(1.5, -0.5, 0 ); 
+	     v1 = new Vector(0, 1,4); 
+	     ray = new Ray(p1, v1); 
+	     result = t1.findIntersections(ray); 
+	     assertEquals("TC01: incorrect intersection", List.of(p2), result);
+	     
+	     //TC02: Point of intersection outside against edge 
+	     p1 = new Point(0.5, -0.5, -2); 
+	     v1 = new Vector(0, 0, 1); 
+	     ray = new Ray(p1, v1); 
+	     result = t1.findIntersections(ray); 
+	     assertNull( "TC02: there should be no intersections", result); 
+	        
+	     //TC03: Point of intersection outside against vertex
+	     p1 = new Point(0.5, 0.5, -2); 
+	     v1 = new Vector(0, 0, 1); 
+	     ray = new Ray(p1, v1); 
+	     result = t1.findIntersections(ray); 
+	     assertNull("TC03: there should be no intersections", result); 
 
-		// ============ EP =================
-		// Inside polygon/triangle
-
-		// Outside against edge
-
-		// Outside against vertex
 	}
 }
 
