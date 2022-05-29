e67916af0f571174395fb9fde8ecaf6e5134ef35
ElishevaWolovitz <elishevawolovitz@gmail.com>
2022-05-29 13:48:13 +0300

    updated material
    did part 2.1

--- src/primitives/Material.java
+++ src/primitives/Material.java
@@ -7,9 +7,17 @@ package primitives;
  * @author elish
  */
 public class Material {
+	/**
+	 * class material with attributes: 
+	 * kD, kD, kT, kR, nShininess all initialized to 0
+	 * and methods: 
+	 * setters for each attribute 
+	 */
 	public Double3 kD = Double3.ZERO;
 	public Double3 kS = Double3.ZERO;
 	public int nShininess = 0;
+	public Double3 kT = Double3.ZERO;
+	public Double3 kR = Double3.ZERO; 
 
 	/**
 	 * setter for kD that recieves Double3
@@ -64,5 +72,46 @@ public class Material {
 	public Material setNShininess(int x) {
 		nShininess = x;
 		return this;
+	}
+	/**
+	 * setter for kT with parameter of type Double3
+	 * @param d3
+	 * @return the object 
+	 */
+	public Material setKT(Double3 d3)
+	{
+		kT = d3; 
+		return this; 
+	}
+	/**
+	 * setter for kR with parameter of type Double3
+	 * @param d3
+	 * @return the object 
+	 */
+	public Material setKR(Double3 d3)
+	{
+		kR = d3; 
+		return this; 
+	}
+	
+	/**
+	 * setter for kT with parameter of type double
+	 * @param d
+	 * @return the object 
+	 */
+	public Material setKT(double d)
+	{
+		kT = new Double3(d); 
+		return this; 
+	}
+	/**
+	 * setter for kR with parameter of type double
+	 * @param d
+	 * @return the object 
+	 */
+	public Material setKR(double d)
+	{
+		kR = new Double3(d); 
+		return this; 
 	}
 }
